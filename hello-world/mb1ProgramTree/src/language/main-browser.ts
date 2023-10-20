fc27fbc23d4a4fe11aa25df14bc93d35fb26db0c
Brendan O'Callaghan <b.ocallaghan@pilz.ie>
2023-10-20 10:17:20 +0000

    added new files

---
+++ hello-world/mb1ProgramTree/src/language/main-browser.ts
@@ -0,0 +1,14 @@
+import { EmptyFileSystem, startLanguageServer } from 'langium';
+import { BrowserMessageReader, BrowserMessageWriter, createConnection } from 'vscode-languageserver/browser.js';
+import { createMb1ProgramTreeServices } from './mb-1-program-tree-module.js';
+
+declare const self: DedicatedWorkerGlobalScope;
+
+const messageReader = new BrowserMessageReader(self);
+const messageWriter = new BrowserMessageWriter(self);
+
+const connection = createConnection(messageReader, messageWriter);
+
+const { shared } = createMb1ProgramTreeServices({ connection, ...EmptyFileSystem });
+
+startLanguageServer(shared);
---
+++ hello-world/mb1ProgramTree/src/language/mb-1-program-tree-validator.ts
@@ -0,0 +1,31 @@
+import type { ValidationAcceptor, ValidationChecks } from 'langium';
+import type { Mb1ProgramTreeAstType, Person } from './generated/ast.js';
+import type { Mb1ProgramTreeServices } from './mb-1-program-tree-module.js';
+
+/**
+ * Register custom validation checks.
+ */
+export function registerValidationChecks(services: Mb1ProgramTreeServices) {
+    const registry = services.validation.ValidationRegistry;
+    const validator = services.validation.Mb1ProgramTreeValidator;
+    const checks: ValidationChecks<Mb1ProgramTreeAstType> = {
+        Person: validator.checkPersonStartsWithCapital
+    };
+    registry.register(checks, validator);
+}
+
+/**
+ * Implementation of custom validations.
+ */
+export class Mb1ProgramTreeValidator {
+
+    checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
+        if (person.name) {
+            const firstChar = person.name.substring(0, 1);
+            if (firstChar.toUpperCase() !== firstChar) {
+                accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
+            }
+        }
+    }
+
+}
